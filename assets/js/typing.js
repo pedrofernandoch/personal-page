c92d65470331eabbfa7ace7faabe0aa56b22dd08
Pedro Christofoletti <pedrofernandoch@gmail.com>
2021-09-07 22:17:41 -0300

    Modularizing, adding timeline and template footer

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,14 +1,3 @@
-// Display Mobile Menu
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
-
-const mobileMenu = () => {
-    menu.classList.toggle('is-active')
-    menuLinks.classList.toggle('active')
-}
-
-menu.addEventListener('click', mobileMenu)
-
 // Typing effect with different words
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
